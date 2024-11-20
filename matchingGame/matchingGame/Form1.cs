87e1dc4a117720a9546ec4bab4cbfe3cc763f563
Dessa Shapiro (RIT Student) <djs9826@rit.edu>
2024-11-20 13:42:44 -0500

    finished matching game code, added win function

--- matchingGame/matchingGame/Form1.cs
+++ matchingGame/matchingGame/Form1.cs
@@ -105,6 +105,20 @@ namespace matchingGame
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+
+                // Check to see if the player won
+                CheckForWinner();
+
+                // If the player clicked two matching icons, keep them 
+                // black and reset firstClicked and secondClicked 
+                // so the player can click another icon
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
                 // If the player gets this far, the player 
                 // clicked two different icons, so start the 
                 // timer (which will wait three quarters of 
@@ -135,6 +149,33 @@ namespace matchingGame
             // clicked, the program knows it's the first click
             firstClicked = null;
             secondClicked = null;
+        }
+
+        /// <summary>
+        /// Check every icon to see if it is matched, by 
+        /// comparing its foreground color to its background color. 
+        /// If all of the icons are matched, the player wins
+        /// </summary>
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel, 
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            // If the loop didn’t return, it didn't find
+            // any unmatched icons
+            // That means the user won. Show a message and close the form
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            Close();
         }
     }
 }
